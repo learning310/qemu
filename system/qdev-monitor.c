6078ed35ec4b71ecb674a4364078ca18a64b53d3
Zhao Liu <zhao1.liu@intel.com>
2024-09-19 14:11:17 +0800

    qdev: Allow qdev_device_add() to add specific category device
    Topology devices need to be created and realized before board
    initialization.
    
    Allow qdev_device_add() to specify category to help create topology
    devices early.
    
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>

--- system/qdev-monitor.c
+++ system/qdev-monitor.c
@@ -632,7 +632,7 @@ const char *qdev_set_id(DeviceState *dev, char *id, Error **errp)
     return prop->name;
 }
 
-DeviceState *qdev_device_add_from_qdict(const QDict *opts,
+DeviceState *qdev_device_add_from_qdict(const QDict *opts, long *category,
                                         bool from_json, Error **errp)
 {
     ERRP_GUARD();
@@ -652,6 +652,10 @@ DeviceState *qdev_device_add_from_qdict(const QDict *opts,
     /* find driver */
     dc = qdev_get_device_class(&driver, errp);
     if (!dc) {
+        return NULL;
+    }
+
+    if (category && !test_bit(*category, dc->categories)) {
         return NULL;
     }
 
@@ -767,12 +771,12 @@ err_del_dev:
 }
 
 /* Takes ownership of @opts on success */
-DeviceState *qdev_device_add(QemuOpts *opts, Error **errp)
+DeviceState *qdev_device_add(QemuOpts *opts, long *category, Error **errp)
 {
     QDict *qdict = qemu_opts_to_qdict(opts, NULL);
     DeviceState *ret;
 
-    ret = qdev_device_add_from_qdict(qdict, false, errp);
+    ret = qdev_device_add_from_qdict(qdict, category, false, errp);
     if (ret) {
         qemu_opts_del(opts);
     }
@@ -897,7 +901,7 @@ void qmp_device_add(QDict *qdict, QObject **ret_data, Error **errp)
         qemu_opts_del(opts);
         return;
     }
-    dev = qdev_device_add(opts, errp);
+    dev = qdev_device_add(opts, NULL, errp);
     if (!dev) {
         /*
          * Drain all pending RCU callbacks. This is done because
